12f03e52463a01b3884b61d0bbe1a8dedc0e652e
furkanoksuz53 <80399254+furkanoksuz53@users.noreply.github.com>
2023-04-05 13:12:47 +0300

    Add files via upload

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.lblCurrentPlayer = new System.Windows.Forms.Label();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblO = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnHowPlay = new System.Windows.Forms.Button();
+            this.btnAboutMe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.a1.Location = new System.Drawing.Point(10, 43);
+            this.a1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(82, 70);
+            this.a1.TabIndex = 0;
+            this.a1.Text = " ";
+            this.a1.UseVisualStyleBackColor = true;
+            this.a1.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // a2
+            // 
+            this.a2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.a2.Location = new System.Drawing.Point(98, 43);
+            this.a2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(82, 70);
+            this.a2.TabIndex = 1;
+            this.a2.Text = " ";
+            this.a2.UseVisualStyleBackColor = true;
+            this.a2.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // a3
+            // 
+            this.a3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.a3.Location = new System.Drawing.Point(186, 43);
+            this.a3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(82, 70);
+            this.a3.TabIndex = 2;
+            this.a3.Text = " ";
+            this.a3.UseVisualStyleBackColor = true;
+            this.a3.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.b3.Location = new System.Drawing.Point(186, 118);
+            this.b3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(82, 70);
+            this.b3.TabIndex = 5;
+            this.b3.Text = " ";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.b2.Location = new System.Drawing.Point(98, 118);
+            this.b2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(82, 70);
+            this.b2.TabIndex = 4;
+            this.b2.Text = " ";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.b1.Location = new System.Drawing.Point(10, 118);
+            this.b1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(82, 70);
+            this.b1.TabIndex = 3;
+            this.b1.Text = " ";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // c3
+            // 
+            this.c3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.c3.Location = new System.Drawing.Point(186, 193);
+            this.c3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(82, 70);
+            this.c3.TabIndex = 8;
+            this.c3.Text = " ";
+            this.c3.UseVisualStyleBackColor = true;
+            this.c3.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // c2
+            // 
+            this.c2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.c2.Location = new System.Drawing.Point(98, 193);
+            this.c2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(82, 70);
+            this.c2.TabIndex = 7;
+            this.c2.Text = " ";
+            this.c2.UseVisualStyleBackColor = true;
+            this.c2.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // c1
+            // 
+            this.c1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.c1.Location = new System.Drawing.Point(10, 193);
+            this.c1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(82, 70);
+            this.c1.TabIndex = 6;
+            this.c1.Text = " ";
+            this.c1.UseVisualStyleBackColor = true;
+            this.c1.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // lblCurrentPlayer
+            // 
+            this.lblCurrentPlayer.AutoSize = true;
+            this.lblCurrentPlayer.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCurrentPlayer.Location = new System.Drawing.Point(297, 49);
+            this.lblCurrentPlayer.Name = "lblCurrentPlayer";
+            this.lblCurrentPlayer.Size = new System.Drawing.Size(155, 21);
+            this.lblCurrentPlayer.TabIndex = 9;
+            this.lblCurrentPlayer.Text = "Sıradaki oyuncu : X";
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblX.Location = new System.Drawing.Point(297, 92);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(95, 21);
+            this.lblX.TabIndex = 10;
+            this.lblX.Text = "Oyuncu X : ";
+            // 
+            // lblO
+            // 
+            this.lblO.AutoSize = true;
+            this.lblO.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblO.Location = new System.Drawing.Point(297, 121);
+            this.lblO.Name = "lblO";
+            this.lblO.Size = new System.Drawing.Size(93, 21);
+            this.lblO.TabIndex = 11;
+            this.lblO.Text = "Oyuncu O :";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnReset.Location = new System.Drawing.Point(377, 219);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 44);
+            this.btnReset.TabIndex = 12;
+            this.btnReset.Text = "Sıfırla";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnClear.Location = new System.Drawing.Point(296, 219);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 44);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Temizle";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnHowPlay
+            // 
+            this.btnHowPlay.Location = new System.Drawing.Point(10, 12);
+            this.btnHowPlay.Name = "btnHowPlay";
+            this.btnHowPlay.Size = new System.Drawing.Size(170, 23);
+            this.btnHowPlay.TabIndex = 14;
+            this.btnHowPlay.Text = "Nasıl Oynanır";
+            this.btnHowPlay.UseVisualStyleBackColor = true;
+            this.btnHowPlay.Click += new System.EventHandler(this.btnHowPlay_Click);
+            // 
+            // btnAboutMe
+            // 
+            this.btnAboutMe.Location = new System.Drawing.Point(186, 12);
+            this.btnAboutMe.Name = "btnAboutMe";
+            this.btnAboutMe.Size = new System.Drawing.Size(82, 23);
+            this.btnAboutMe.TabIndex = 15;
+            this.btnAboutMe.Text = "Hakkında";
+            this.btnAboutMe.UseVisualStyleBackColor = true;
+            this.btnAboutMe.Click += new System.EventHandler(this.btnAboutMe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 274);
+            this.Controls.Add(this.btnAboutMe);
+            this.Controls.Add(this.btnHowPlay);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblO);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.lblCurrentPlayer);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button a1;
+        private Button a2;
+        private Button a3;
+        private Button b3;
+        private Button b2;
+        private Button b1;
+        private Button c3;
+        private Button c2;
+        private Button c1;
+        private Label lblCurrentPlayer;
+        private Label lblX;
+        private Label lblO;
+        private Button btnReset;
+        private Button btnClear;
+        private Button btnHowPlay;
+        private Button btnAboutMe;
+    }
+}
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Reset();
+        }
+        int count = 0;
+        int winX = 0;
+        int winO = 0;
+                
+        private void c3_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;            
+            button.Text = count % 2 == 0 ? "X" : "O";
+            button.Enabled = false;
+            lblCurrentPlayer.Text = count % 2 == 0 ? "Sıradaki oyuncu : O" : "Sıradaki oyuncu : X";
+            count++;
+            CheckWinner();            
+            
+        }
+        private void CheckWinner()
+        {
+            if( 
+                a1.Text + a2.Text+  a3.Text == "XXX" ||
+                b1.Text + b2.Text + b3.Text == "XXX" ||
+                c1.Text + c2.Text + c3.Text == "XXX" ||
+                a1.Text + b1.Text + c1.Text == "XXX" ||
+                a2.Text + b2.Text + c2.Text == "XXX" ||
+                a3.Text + b3.Text + c3.Text == "XXX" ||
+                a1.Text + b2.Text + c3.Text == "XXX" ||
+                a3.Text + b2.Text + c1.Text == "XXX") 
+            {
+                MessageBox.Show("Oyuncu X kazandı");
+                winX++;
+                lblX.Text = "Oyuncu X : " + winX;
+                Clear();
+                
+            }
+            else if(   
+                a1.Text + a2.Text + a3.Text == "OOO" ||
+                b1.Text + b2.Text + b3.Text == "OOO" ||
+                c1.Text + c2.Text + c3.Text == "OOO" ||
+                a1.Text + b1.Text + c1.Text == "OOO" ||
+                a2.Text + b2.Text + c2.Text == "OOO" ||
+                a3.Text + b3.Text + c3.Text == "OOO" ||
+                a1.Text + b2.Text + c3.Text == "OOO" ||
+                a3.Text + b2.Text + c1.Text == "OOO") 
+            {
+                MessageBox.Show("Oyuncu O kazandı");
+                winO++;
+                lblO.Text ="Oyuncu O : " +winO ;
+                Clear();
+            }
+            else if( count + 1 == 9)
+            {
+                MessageBox.Show("Berabere");
+                Clear();
+            }
+            else { }
+
+        }
+        private void Clear()
+        {
+            a1.Text = string.Empty;            
+            a2.Text = string.Empty;
+            a3.Text = string.Empty;
+            b1.Text = string.Empty;
+            b2.Text = string.Empty;
+            b3.Text = string.Empty;
+            c1.Text = string.Empty;
+            c2.Text = string.Empty;
+            c3.Text = string.Empty;
+            a1.Enabled = true;
+            a2.Enabled = true;
+            a3.Enabled = true;
+            b1.Enabled = true; 
+            b2.Enabled = true;
+            b3.Enabled = true;
+            c1.Enabled = true; 
+            c2.Enabled = true;
+            c3.Enabled = true;
+            count = 0;
+            lblCurrentPlayer.Text = "Sıradaki oyuncu : X";
+
+        }
+        private void Reset()
+        {
+            Clear();
+            winO = 0;
+            winX = 0;
+            lblX.Text = "Oyuncu X : " + winX;
+            lblO.Text = "Oyuncu O : " + winO;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void btnHowPlay_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("·Oyuna kimin başlayacağına ve hangi sembolü kullanacağına karar verilir. (Yazı-tura / taş-kağıt-makas gibi yöntemler kullanılabilir.)\r\n\r\n·Oyunculardan biri X, diğeri O sembolünü kullanmalıdır.\r\n\r\n·Oyun sonunda 3 aynı sembolü arka arkaya sıralayan oyuncu, 3 sembolün üzerine çizik atar ve oyunu kazanır.\r\n\r\n·Oyun sonunda 3 aynı sembol arka arkaya sıralanamamış ise oyun beraberedir.");
+        }
+
+        private void btnAboutMe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Created by Vulture53 on April 5, 2023");
+        }
+    }
+}
